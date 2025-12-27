645847ea542dc7bfababada1e09e6802590f8342
Ragul J <Ragul.J@ust.trumio.ai>
2025-12-27 12:29:00 +0530

    Main code fixed

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
-from app.routers import ai_tools, reviews, admin
+from routers import auth,review,tool,user
 
 app = FastAPI(title="AI Tool Finder Backend")
 
 # Include routers
-app.include_router(ai_tools.router)
-app.include_router(reviews.router)
-app.include_router(admin.router)
+app.include_router(auth.router)
+app.include_router(review.router)
+app.include_router(tool.router)
+app.include_router(user.router)
+
 
 @app.get("/")
 def root():
-<<<<<<< HEAD
     return {"message": "Welcome to AI Tool Finder Backend"}
-=======
-    return {"message": "Welcome to AI Tool Finder Backend"}
->>>>>>> e1df838ce77c58b6d2cf61546d81d077f00f7451
