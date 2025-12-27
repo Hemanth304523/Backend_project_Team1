243539bf56cba8946fbb65bd0ab9df917f7d3d7a
RR <raveeraghul.c@ust.trumio.ai>
2025-12-27 10:27:30 +0530

    AITool Models Added

--- model.py
+++ model.py
@@ -12,3 +12,19 @@ class Users(Base):
     last_name = Column(String)
     hashed_password = Column(String)
     role = Column(String)
+    
+class PricingType(enum.Enum):
+
+    FREE = "FREE"
+    PAID = "PAID"
+    SUBSCRIPTION = "SUBSCRIPTION"
+class AITool(Base):
+
+    __tablename__ = "ai_tools"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    tool_name = Column(String(150), nullable=False, index=True)
+    use_case = Column(Text)
+    category = Column(String(100), index=True)
+    pricing_type = Column(Enum(PricingType), nullable=False)
+    avg_rating = Column(Float, default=0.0)
+ 
