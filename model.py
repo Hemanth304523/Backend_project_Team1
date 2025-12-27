d453bff403e78f89c2ae20cd59bfafe03e0384b4
Amal <Amal.Falah@ust.trumio.ai>
2025-12-27 10:37:08 +0530

    review model added

--- model.py
+++ model.py
@@ -2,7 +2,14 @@ import enum
 from uuid import uuid4
 from database import Base
 from sqlalchemy import UUID, Column, Float, Integer, String, Boolean, ForeignKey, Text
+import enum
+from uuid import uuid4
+from datetime import datetime
  
+from sqlalchemy import Column, Integer, Text, DateTime, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+ 
+from database import Base
  
 class Users(Base):
     __tablename__ = 'users'
@@ -29,4 +36,25 @@ class AITool(Base):
     category = Column(String(100), index=True)
     pricing_type = Column(enum.Enum(PricingType), nullable=False)
     avg_rating = Column(Float, default=0.0)
+
+ 
+class ReviewStatus(enum.Enum):
+    PENDING = "PENDING"
+    APPROVED = "APPROVED"
+    REJECTED = "REJECTED"
+ 
+class Review(Base):
+    __tablename__ = "reviews"
+ 
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+ 
+    tool_id = Column(UUID(as_uuid=True), ForeignKey("ai_tools.id", ondelete="CASCADE"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"))
+ 
+    user_rating = Column(Integer, nullable=False)
+    comment = Column(Text)
+    approval_status = Column(Enum(ReviewStatus), default=ReviewStatus.PENDING)
+ 
+ 
+ 
  
